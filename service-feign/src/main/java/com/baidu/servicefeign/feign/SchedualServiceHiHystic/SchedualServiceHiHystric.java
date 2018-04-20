3c5fddecbb184af52853a1c8fb4496a365dcc178
sxiny <748701592@qq.com>
2018-04-20 15:26:35 +0800

    第四次修改springcloud

--- service-feign/src/main/java/com/baidu/servicefeign/feign/SchedualServiceHiHystic/SchedualServiceHiHystric.java
+++ service-feign/src/main/java/com/baidu/servicefeign/feign/SchedualServiceHiHystic/SchedualServiceHiHystric.java
@@ -1,7 +1,12 @@
-package com.baidu.servicefeign.feign;
+package com.baidu.servicefeign.feign.SchedualServiceHiHystic;
 
+import com.baidu.servicefeign.feign.SchedualServiceHi;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author yuxiaoxing
+ * @date 2018/4/20 13:56
+ */
 @Component
 public class SchedualServiceHiHystric implements SchedualServiceHi {
     @Override
